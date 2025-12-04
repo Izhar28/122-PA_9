e1ca2bbd56b7239c54047d25bea55d0dd5572fad
cortlocke <cort.locke@wsu.edu>
2025-12-03 23:15:15 -0800

    Update Knight.hpp

--- Final Game Files/Knight.hpp
+++ Final Game Files/Knight.hpp
@@ -1,3 +1,5 @@
+// This Header File contains the class of the Knight piece in chess.
+// This class assignes the color of the piece and determines the movement pattern of the piece
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <cstdlib>
@@ -12,4 +14,5 @@ public:
 	~Knight();
 
 	void movementSquares(Board& gameBoard) override;
+
 };
